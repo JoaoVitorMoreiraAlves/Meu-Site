5b1996e5572b32af4a33053b46ba437eb06b95eb
João Vitor <joaomoreiraalves9@gmail.com>
2023-10-30 11:45:20 -0300

    Projeto do Site

--- menu-mobile.js
+++ menu-mobile.js
@@ -15,3 +15,36 @@ menuMobile.addEventListener('click', ()=>{
 overlay.addEventListener('click', () => {
     menuMobile.classList.remove('abrir-menu-mobile');
 });
+
+
+
+//Enviando o email sem acessar o site formspree através do AJAX
+const formulario = document.getElementById('form');
+
+formulario.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Obtenha os dados do formulário
+  const formData = new FormData(formulario);
+
+  // Use AJAX para enviar os dados
+  fetch(formulario.action, {
+    method: formulario.method,
+    body: formData,
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+
+        alert("Mensagem enviada com sucesso!")
+
+    // Limpe os campos do formulário
+    formulario.reset();
+  })
+  .catch(error => {
+    // Lidar com erros, se necessário
+    console.error(error);
+  });
+});
