86e644d6c28b8032bb692ed34ee368a325954c51
João Vitor <joaomoreiraalves9@gmail.com>
2023-11-01 22:08:40 -0300

    Finalizando Site

--- menu-mobile.js
+++ menu-mobile.js
@@ -3,20 +3,20 @@ let menuMobile = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu-mobile')
 
 
-
+//Evento para Abrir o Menu Mobile quando for clicado no icone que o representa
 btnMenu.addEventListener('click', ()=>{
     menuMobile.classList.add('abrir-menu-mobile');
 })
 
+//Evento para Abrir o Menu Mobile quando for clicado no icone que o representa
 menuMobile.addEventListener('click', ()=>{
     menuMobile.classList.remove('abrir-menu-mobile');
 })
 
+//Evento para Abrir o Overlay quando for clicado no icone que o representa
 overlay.addEventListener('click', () => {
     menuMobile.classList.remove('abrir-menu-mobile');
 });
-
-
 
 //Enviando o email sem acessar o site formspree através do AJAX
 const formulario = document.getElementById('form');
@@ -38,7 +38,7 @@ formulario.addEventListener('submit', function (e) {
   .then(response => response.json())
   .then(data => {
 
-        alert("Mensagem enviada com sucesso!")
+      alert("Mensagem enviada com sucesso!")
 
     // Limpe os campos do formulário
     formulario.reset();
