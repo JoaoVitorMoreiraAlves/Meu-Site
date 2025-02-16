c7b4c410ce6d3364029bea47f552502a57c41c18
batalhanavalfatec <jovimoalsw@gmail.com>
2025-02-16 16:59:13 -0300

    Adicionando funcionalidade de pagina em inglês e português

--- menu-mobile.js
+++ menu-mobile.js
@@ -22,29 +22,100 @@ overlay.addEventListener('click', () => {
 const formulario = document.getElementById('form');
 
 formulario.addEventListener('submit', function (e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  // Obtenha os dados do formulário
-  const formData = new FormData(formulario);
+    // Obtenha os dados do formulário
+    const formData = new FormData(formulario);
 
-  // Use AJAX para enviar os dados
-  fetch(formulario.action, {
-    method: formulario.method,
-    body: formData,
-    headers: {
-      'Accept': 'application/json'
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
+    // Use AJAX para enviar os dados
+    fetch(formulario.action, {
+      method: formulario.method,
+      body: formData,
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
 
-      alert("Mensagem enviada com sucesso!")
+        alert("Mensagem enviada com sucesso!")
 
-    // Limpe os campos do formulário
-    formulario.reset();
-  })
-  .catch(error => {
-    // Lidar com erros, se necessário
-    console.error(error);
-  });
+      // Limpe os campos do formulário
+      formulario.reset();
+    })
+    .catch(error => {
+      // Lidar com erros, se necessário
+      console.error(error);
+    });
 });
+
+//Script para mudar o idioma do site
+function toggleLanguagePopup() {
+    var popup = document.getElementById('language-popup');
+    if (popup.style.display === 'none' || popup.style.display === '') {
+        popup.style.display = 'block';
+    } else {
+        popup.style.display = 'none';
+    }
+}
+
+function changeLanguage(lang) {
+    var elements = document.querySelectorAll('[data-lang]');
+    elements.forEach(function(element) {
+        element.innerHTML = element.getAttribute('data-lang-' + lang);
+    });
+    var placeholders = document.querySelectorAll('[data-lang-placeholder]');
+      placeholders.forEach(function(element) {
+          element.placeholder = element.getAttribute('data-lang-placeholder-' + lang);
+      });
+    
+    var titles = document.querySelectorAll('[data-lang-title]');
+    titles.forEach(function(element) {
+        element.title = element.getAttribute('data-lang-title-' + lang);
+    });
+
+    var inputs = document.querySelectorAll('[data-lang]');
+    inputs.forEach(function(element) {
+        if (element.tagName.toLowerCase() === 'input' && element.type === 'submit') {
+            element.value = element.getAttribute('data-lang-' + lang);
+        }
+    });
+    // Atualizar a bandeira ativa
+    var activeFlag = document.getElementById('active-flag');
+    if (lang === 'pt-br') {
+        activeFlag.style.backgroundImage = "url('/images/pt-bandeira.png')"; //Imagem da bandeira brasileira
+        document.title = "Portifólio - João Vitor"; // Título em português
+    } else if (lang === 'en') {
+        activeFlag.style.backgroundImage = "url('/images/en-bandeira.png')"; // Imagem da bandeira americana
+        document.title = "Portfolio - João Vitor"; // Título em inglês
+    }
+
+    // Fechar o popup após a seleção
+    document.getElementById('language-popup').style.display = 'none';
+}
+
+//Baixar curriculo da lingua certa
+function baixa_cv() {
+    // Verifica o valor do atributo data-lang do título do botão de download
+    const lang = document.querySelector('.btn-download').getAttribute('title') === 'Download Curriculum Vitae' ? 'en' : 'pt-br';
+
+    // Define os caminhos dos arquivos com base no idioma
+    let cvFile = "";
+    let fileName = "";
+    
+    if (lang === "en") {
+        cvFile = "Curriculo/CurriculoENG_JoãoVitorMoreiraAlves_2025.rar"; // Caminho do arquivo
+        fileName = "CurriculumVitae_JoãoVitorMoreiraAlves.rar"; // Nome personalizado para download
+    } else {
+        cvFile = "Curriculo/CurriculoPTBR_JoãoVitorMoreiraAlves_2025.rar"; // Caminho do arquivo
+        fileName = "Curriculo_JoãoVitorMoreiraAlves.rar"; // Nome personalizado para download
+    }
+    
+    // Cria um link para download
+    const link = document.createElement("a");
+    link.href = cvFile;
+    link.download = fileName; // Define o nome personalizado do arquivo
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
